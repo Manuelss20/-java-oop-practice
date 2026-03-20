e1a3def0a6a2bddb1d4263d12c92026bfd10731c
Manuel Salgado Enríquez <manuelsss20@Manuels-MacBook-Air.local>
2026-03-19 22:06:45 -0600

    Se añadió un ejercicio de un pequeño sistema de calificaciones

---
+++ LibroCalificaciones/src/librocalificaciones/LibroCalificaciones.java
@@ -0,0 +1,16 @@
+
+// La clase LibroCalificaciones con un constructor para inicializar el nombre del curso.
+
+package librocalificaciones;
+
+/* @author Salgado Enríquez Manuel, Zaldivar Ortega Juan Carlos */
+
+public class LibroCalificaciones {
+    
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
